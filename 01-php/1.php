4c36ff8b68c72f2f5441b87dbf4c9562f66b807d
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-04-10 12:04:55 +0200

    changing the http headers

--- 01-php/1.php
+++ 01-php/1.php
@@ -1,11 +1,13 @@
 <?php
 
+    Header("Content-type: text/plain");
+
     $anArray['first'] = 1;
     $anArray['second'] = 2;
     $anArray['third'] = 3;
 
     foreach($anArray as $k => $v) {
-        echo "{$k}: $v \n";
+        echo "<strong>{$k}</strong>: {$v} <br>\n";
     }
 
 
