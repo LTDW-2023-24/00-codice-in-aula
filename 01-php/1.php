a40b080ca94a9085ccc428a2ed79e4a2487f5bec
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-04-11 16:58:51 +0200

    sssss

--- 01-php/1.php
+++ 01-php/1.php
@@ -1,10 +1,12 @@
 <?php
 
-    Header("Content-type: text/plain");
-
     $anArray['first'] = 1;
     $anArray['second'] = 2;
     $anArray['third'] = 3;
+
+    $anArray[123] = 4;
+
+    $anArray[] = 5;
 
     foreach($anArray as $k => $v) {
         echo "<strong>{$k}</strong>: {$v} <br>\n";
